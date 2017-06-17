e3ac5717fda77c5659a84ad56a4097512b71006c
Andrew Phung <andrewphung@Andrews-MacBook-Pro.local>
2017-06-17 16:40:43 -0500

    added canvas and simple animation

--- javascript/get_data.js
+++ javascript/get_data.js
@@ -1,3 +1,34 @@
-$.getJSON("assets/test.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		end_ang = 0
+
+	window.addEventListener('resize', resizeCanvas, false)
+	resizeCanvas()
+
+	var sleep
+	$.getJSON("assets/test.json", function(json) {
+		sleep = json
+	})
+	console.log(sleep)
+	
+	setInterval(function() {
+		requestAnimationFrame(draw)
+	}, 1000/60)
+	
+	function draw() {
+		var width = canvas.width
+		var height = canvas.height
+		context.clearRect(0,0,600,600)
+		context.beginPath()
+		context.arc(100, 100, 20, 0, end_ang)
+		context.stroke()
+		end_ang += .1
+	}
+
+	function resizeCanvas() {
+		canvas.width = window.innerWidth
+		canvas.height = window.innerHeight
+		draw()
+	}
+}
